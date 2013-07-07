aec2e368d9c977625a75354bc75784509fbd748d
XiaoxuShen <shenxiaoxu22@gmail.com>
2013-07-07 12:43:58 -0400

    SortArray
    You are given an array of 1's 2's and 3's. Sort this list so the 1's
    are first. 2's come second. 3's come third

---
+++ src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author XiaoxuShen
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+         *You are given an array of 1's 2's and 3's. Sort this list so the 1's are first, 
+         * the 2's come second, and the 3's come third. 
+
+        Ex: Input [1, 3, 3, 2, 1] 
+        Output [1, 1, 2, 3, 3] 
+
+            But there is a catch!! The algorithm must be one pass, 
+          which means no merge/quick sort. Also no extra list allocations are allowed, which means no bucket/radix/counting sorts. 
+
+            You are only permitted to swap elements.
+            * solution: dutch national flag problem.
+         */
+    }
+}
